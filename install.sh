bc801256dd6e33e8674bb644838479246e783a32
trevormurphy424 <83364510+trevormurphy424@users.noreply.github.com>
2021-11-11 22:55:04 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-
+adduser minecraft
+apt update
+apt upgrade
+apt install openjdk-16-jre
+cd ~
+touch start.sh
+echo "java -Xmx6G -Xms1G -jar server.jar nogui" >> start.sh
+mkdir server
+cd server
+wget https://api.pl3x.net/v2/purpur/1.17.1/latest/download.jar
+mv download.jar server.jar
